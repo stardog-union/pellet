1748a2f6c10a4a3d73ecfa55c48a320f1f3a8592
Pavel Klinov <pavel.klinov@gmail.com>
2015-04-06 15:29:00 +0200

    an updated test which concurrently queries for all object/data property values for an individual

--- test/src/test/java/com/clarkparsia/pellet/test/jena/ConcurrencyTest.java
+++ test/src/test/java/com/clarkparsia/pellet/test/jena/ConcurrencyTest.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -21,6 +23,7 @@ import com.hp.hpl.jena.ontology.ConversionException;
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 
 /**
  * Verifies that Pellet doesn't throw any exceptions when doing concurrent ABox
@@ -32,26 +35,53 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
  */
 public class ConcurrencyTest {
 
-	//private static final Logger LOGGER_ = LoggerFactory.getLogger(ConcurrencyTest.class);
-	
 	private static final String ONTOLOGY_PATH_ = "/test/data/concurrency/vicodi.ttl";
 	
 	private static final int THREAD_NUMBER = 10;
 	
+	private static final Iterable<Individual> POISON = Collections.emptyList();
+	
+	private static final int BATCH_SIZE = 10;
+	
 	@Test
 	public void concurrentDLQueries() throws Exception {
 		// create Pellet reasoner
-        //Reasoner reasoner = PelletReasonerFactory.theInstance().create();
+		System.err.println("Loading the ontology");
 		OntModel ontModel = loadOntologyModel(ONTOLOGY_PATH_);
 		
 		// force classification and realization
+		System.err.println("Realizing the ontology");
+		
 		((PelletInfGraph) ontModel.getGraph()).realize();
+		BlockingQueue<Iterable<Individual>> toDo = new ArrayBlockingQueue<Iterable<Individual>>(2 * THREAD_NUMBER);
 		
 		// launching threads which will concurrently run type queries on instances
 		ExecutorService pool = Executors.newFixedThreadPool(THREAD_NUMBER);
 		
 		for (int i = 0; i < THREAD_NUMBER; i++) {
-			pool.execute(new QueryRunner(ontModel));
+			pool.execute(new QueryRunner(ontModel, toDo));
+		}
+		
+		// adding the individuals to the processing queue for the threads to process concurrently
+		Iterator<Individual> individuals = ontModel.listIndividuals();
+		
+		while (individuals.hasNext()) {
+			List<Individual> batch = new ArrayList<Individual>(BATCH_SIZE);
+			
+			for (int i = 0; i < BATCH_SIZE && individuals.hasNext(); i++) {
+				Individual ind = individuals.next();
+				
+				batch.add(ind);
+			}
+			
+			if (!batch.isEmpty()) {
+				toDo.put(batch);
+			}
+		}
+		
+		// letting the threads die...
+		for (int i = 0; i < THREAD_NUMBER; i++) {
+			toDo.put(POISON);
 		}
 		
 		pool.shutdown();
@@ -80,32 +110,54 @@ public class ConcurrencyTest {
 
 		private final OntModel model_;
 		
-		QueryRunner(OntModel model) {
+		private final BlockingQueue<Iterable<Individual>> toDo_;
+		
+		QueryRunner(OntModel model, BlockingQueue<Iterable<Individual>> toDo) {
 			model_ = model;
+			toDo_ = toDo;
 		}
 		
 		@Override
 		public void run() {
-			// listing all individuals
-			Iterator<Individual> indIter = model_.listIndividuals();
-			List<Individual> indList = new ArrayList<Individual>();
-			
-			while (indIter.hasNext()) {
-				indList.add(indIter.next());
-			}
-			
-			Collections.shuffle(indList);
-			
-			// now running the queries
-			for (Individual ind : indList.subList(0, Math.min(20000, indList.size() - 1))) {
-				printIterator(ind.listOntClasses(false), Thread.currentThread().getName());
+			for (;;) {
+				Iterable<Individual> batch = null;
+				
+				try {
+					batch = toDo_.take();
+				} catch (InterruptedException e) {
+					break;
+				}
+				
+				if (batch == POISON) {
+					// we're done
+					break;
+				}
+				
+				for (Individual ind : batch) {
+					// querying for all object property values for each individual	
+					Iterator<? extends Property> propertyIter = model_.listObjectProperties();
+
+					while (propertyIter.hasNext()) {
+						Property property = propertyIter.next();
+
+						printIterator(ind.listPropertyValues(property), Thread.currentThread().getName() + ": " + ind.getLocalName() + " -- " + property.getLocalName() + " --> ");
+					}
+					
+					// querying for all data property values for each individual	
+					propertyIter = model_.listDatatypeProperties();
+
+					while (propertyIter.hasNext()) {
+						Property property = propertyIter.next();
+
+						printIterator(ind.listPropertyValues(property), Thread.currentThread().getName() + ": " + ind.getLocalName() + " -- " + property.getLocalName() + " --> ");
+					}
+				}
 			}
 		}
 		
 		public void printIterator(Iterator<?> iterator, String threadId) {
 			if (iterator.hasNext()) {
 				while (iterator.hasNext()) {
-					//LOGGER_.trace("{}: {}", threadId, iterator.next());
 					try {
 						System.err.println(threadId + ": "+ iterator.next());
 					} catch (ConversionException e) {
