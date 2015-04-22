a53a2166d9f0cb78e2bc83e4c9bfb81d563bd99f
Evren Sirin <evren@clarkparsia.com>
2015-04-22 13:03:16 -0400

    Add Protege plugin code and the plugin binaries generated for Pellet 2.3.0

---
+++ protege/src/main/java/com/clarkparsia/protege/plugin/incremental/PelletIncrementalReasonerFactory.java
@@ -0,0 +1,35 @@
+// Copyright (c) 2006 - 2008, Clark & Parsia, LLC. <http://www.clarkparsia.com>
+// This source code is available under the terms of the Affero General Public
+// License v3.
+//
+// Please see LICENSE.txt for full license terms, including the availability of
+// proprietary exceptions.
+// Questions, comments, or requests for clarification: licensing@clarkparsia.com
+
+package com.clarkparsia.protege.plugin.incremental;
+
+import org.protege.editor.owl.model.inference.AbstractProtegeOWLReasonerInfo;
+import org.semanticweb.owlapi.reasoner.BufferingMode;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+
+import com.clarkparsia.modularity.PelletIncremantalReasonerFactory;
+
+/**
+ * @author Evren Sirin
+ */
+public class PelletIncrementalReasonerFactory extends AbstractProtegeOWLReasonerInfo  {
+	/**
+     * {@inheritDoc}
+     */
+    public OWLReasonerFactory getReasonerFactory() {
+	    return PelletIncremantalReasonerFactory.getInstance();
+    }
+
+
+	/**
+     * {@inheritDoc}
+     */
+    public BufferingMode getRecommendedBuffering() {
+	    return BufferingMode.BUFFERING;
+    }
+}
---
+++ protege/src/main/java/com/clarkparsia/protege/plugin/pellet/PelletReasonerFactory.java
@@ -0,0 +1,36 @@
+package com.clarkparsia.protege.plugin.pellet;
+
+import org.mindswap.pellet.PelletOptions;
+import org.protege.editor.owl.model.inference.AbstractProtegeOWLReasonerInfo;
+import org.semanticweb.owlapi.reasoner.BufferingMode;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+
+/**
+ * 
+ * @author Evren Sirin
+ */
+public class PelletReasonerFactory extends AbstractProtegeOWLReasonerInfo {
+	static {
+		// true = (default) Non DL axioms will be ignored (eg as use of complex
+		// roles in cardinality restrictions)
+		// false = pellet will throw an exception if non DL axioms are included
+		PelletOptions.IGNORE_UNSUPPORTED_AXIOMS = false;
+
+		PelletOptions.SILENT_UNDEFINED_ENTITY_HANDLING = true;
+	}
+
+	/**
+     * {@inheritDoc}
+     */
+    public OWLReasonerFactory getReasonerFactory() {
+	    return com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory.getInstance();
+    }
+
+
+	/**
+     * {@inheritDoc}
+     */
+    public BufferingMode getRecommendedBuffering() {
+	    return BufferingMode.BUFFERING;
+    }
+}
