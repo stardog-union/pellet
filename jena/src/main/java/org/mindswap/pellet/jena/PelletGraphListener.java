1317ae1be6f342a53886d824e1774d07ad7a5b67
Sebastian Siemoleit <sebastian.siemoleit@inmindcomputing.de>
2015-06-10 14:48:58 +0800

    Enabling incremental deletion of datatype property assertions.

--- jena/src/main/java/org/mindswap/pellet/jena/PelletGraphListener.java
+++ jena/src/main/java/org/mindswap/pellet/jena/PelletGraphListener.java
@@ -13,6 +13,7 @@ import java.util.Set;
 
 import org.mindswap.pellet.KnowledgeBase;
 import org.mindswap.pellet.PelletOptions;
+import org.mindswap.pellet.utils.ATermUtils;
 import org.mindswap.pellet.utils.iterator.IteratorUtils;
 
 import aterm.ATermAppl;
@@ -23,340 +24,338 @@ import com.hp.hpl.jena.graph.GraphListener;
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.graph.compose.Dyadic;
-import com.hp.hpl.jena.graph.compose.MultiUnion;
 import com.hp.hpl.jena.graph.compose.Polyadic;
 import com.hp.hpl.jena.reasoner.InfGraph;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 /**
- * A graph listener that listens to graph change events and if possible
- * processes the change event. The listener is passed a possibly union
- * graph but the listener is attached only to leaf (non-union) graphs.
- * The listener keeps track which graph is changed and also checks
- * if subgraphs are added or removed from the root graph. 
- * 
+ * A graph listener that listens to graph change events and if possible processes the change event. The listener is
+ * passed a possibly union graph but the listener is attached only to leaf (non-union) graphs. The listener keeps track
+ * which graph is changed and also checks if subgraphs are added or removed from the root graph.
+ *
  * @author Evren Sirin
  */
 public class PelletGraphListener implements GraphListener {
-	// KB object - used for incremental ABox changes
-	private KnowledgeBase	kb;
-	
-	private Graph 			rootGraph;
+  // KB object - used for incremental ABox changes
+  private final KnowledgeBase kb;
 
-	private Set<Graph>		leafGraphs;
-	
-	private Set<Graph>		changedGraphs;
-	
-	private boolean			statementDeleted;
-	
-	private boolean 		enabled;
+  private final Graph rootGraph;
 
-	public PelletGraphListener(Graph rootGraph, KnowledgeBase kb, boolean enabled) {
-		this.rootGraph = rootGraph;
-		this.kb = kb;
-		this.enabled = enabled;
-		
-		leafGraphs = CollectionUtils.makeSet();
-		changedGraphs = CollectionUtils.makeSet();
-		
-		statementDeleted = false;
-		
-		if (enabled) {
-			collectLeafGraphs( rootGraph, Collections.<Graph>emptySet() );
-		}
-	}
-	
-	public void setEnabled(boolean enabled) {
-		if (this.enabled == enabled) {
-			return;
-		}
-		
-		this.enabled = enabled;
+  private Set<Graph> leafGraphs;
 
-		leafGraphs.clear();
-		changedGraphs.clear();
+  private final Set<Graph> changedGraphs;
 
-		statementDeleted = false;
-		
-		if (enabled) {
-			collectLeafGraphs( rootGraph, Collections.<Graph>emptySet() );
-		}
-		else {
-			for( Graph graph : leafGraphs ) {
-				graph.getEventManager().unregister( this );
-			}
-		}
-	}
-	
-	private void addABoxTriple(Triple t) {
-		// Convert the Jena nodes to ATermAppl
-		ATermAppl s = JenaUtils.makeATerm( t.getSubject() );
-		ATermAppl o = JenaUtils.makeATerm( t.getObject() );
+  private boolean statementDeleted;
 
-		// check if this is a type assertion
-		if( t.getPredicate().equals( RDF.type.asNode() ) ) {
-			// check if this is a new individual
-			if( !kb.getIndividuals().contains( s ) )
-				kb.addIndividual( s );
+  private boolean enabled;
 
-			// add the type
-			kb.addType( s, o );
-		}
-		else {
-			// check if the subject is a new individual
-			if( !kb.getIndividuals().contains( s ) )
-				kb.addIndividual( s );
+  public PelletGraphListener(final Graph rootGraph, final KnowledgeBase kb, final boolean enabled) {
+    this.rootGraph = rootGraph;
+    this.kb = kb;
+    this.enabled = enabled;
 
-			// check if the object is a new individual
-			if( !t.getObject().isLiteral() && !kb.getIndividuals().contains( o ) )
-				kb.addIndividual( o );
+    leafGraphs = CollectionUtils.makeSet();
+    changedGraphs = CollectionUtils.makeSet();
 
-			ATermAppl p = JenaUtils.makeATerm( t.getPredicate() );
-			// add the property value
-			kb.addPropertyValue( p, s, o );
-		}
-	}
+    statementDeleted = false;
 
-	private void deleteABoxTriple(Triple t) {
-		ATermAppl s = JenaUtils.makeATerm( t.getSubject() );
-		ATermAppl o = JenaUtils.makeATerm( t.getObject() );
+    if (enabled) {
+      collectLeafGraphs(rootGraph, Collections.<Graph> emptySet());
+    }
+  }
 
-		// check if this is a type assertion
-		if( t.getPredicate().equals( RDF.type.asNode() ) ) {
-			if( kb.isIndividual( s ) )
-				kb.removeType( s, o );
-		}
-		else {
-			// check if the subject is a new individual
-			if( kb.isIndividual( s ) && kb.isIndividual( o ) ) {
-				ATermAppl p = JenaUtils.makeATerm( t.getPredicate() );
-				// add the property value
-				kb.removePropertyValue( p, s, o );
-			}
-		}
-	}
-	
-	private void collectLeafGraphs(Graph graph, Set<Graph> prevLeaves) {
-		if( graph instanceof Polyadic ) {
-			Polyadic union = ((Polyadic) graph);
-			if( union.getBaseGraph() != null )
-				collectLeafGraphs( union.getBaseGraph(), prevLeaves );
+  private void addABoxTriple(final Triple t) {
+    // Convert the Jena nodes to ATermAppl
+    final ATermAppl s = JenaUtils.makeATerm(t.getSubject());
+    final ATermAppl o = JenaUtils.makeATerm(t.getObject());
 
-			for( Iterator<Graph> i = union.getSubGraphs().iterator(); i.hasNext(); )
-				collectLeafGraphs( i.next(), prevLeaves );
-		}
-		else if( graph instanceof Dyadic ) {
-			Dyadic dyadic = ((Dyadic) graph);
-			if( dyadic.getL() instanceof Graph )
-				collectLeafGraphs( (Graph) dyadic.getL(), prevLeaves );
+    // check if this is a type assertion
+    if (t.getPredicate().equals(RDF.type.asNode())) {
+      // check if this is a new individual
+      if (!kb.getIndividuals().contains(s))
+        kb.addIndividual(s);
 
+      // add the type
+      kb.addType(s, o);
+    } else {
+      // check if the subject is a new individual
+      if (!kb.getIndividuals().contains(s))
+        kb.addIndividual(s);
 
-			if( dyadic.getR() instanceof Graph )
-				collectLeafGraphs( (Graph) dyadic.getR(), prevLeaves );
-		}
-		else if( graph instanceof InfGraph ) {
-			collectLeafGraphs( ((InfGraph) graph).getRawGraph(), prevLeaves );
-		}
-		else if( leafGraphs.add( graph ) && !prevLeaves.contains( graph ) ) {
-			changedGraphs.add( graph );
-			
-			graph.getEventManager().register( this );
-		}
-	}
-	
-	/**
-	 * Checks if the graph can be u[dated incrementally
-	 * 
-	 * @return
-	 */
-	private boolean canUpdateIncrementally(Graph g) {
-		return PelletOptions.PROCESS_JENA_UPDATES_INCREMENTALLY && !statementDeleted && !changedGraphs.contains( g );
-	}
+      // check if the object is a new individual
+      if (!t.getObject().isLiteral() && !kb.getIndividuals().contains(o))
+        kb.addIndividual(o);
 
-	public void dispose() {
-		for( Graph graph : leafGraphs ) {
-			graph.getEventManager().unregister( this );
-		}
-		
-		leafGraphs.clear();
-		changedGraphs.clear();
+      final ATermAppl p = JenaUtils.makeATerm(t.getPredicate());
+      // add the property value
+      kb.addPropertyValue(p, s, o);
+    }
+  }
 
-		statementDeleted = false;
-	}
-	
-	public boolean isChanged() {
-		if( statementDeleted || !changedGraphs.isEmpty() ) {
-			return true;
-		}
-		
-		getChangedGraphs();
-		
-		return statementDeleted || !changedGraphs.isEmpty();
-	}
+  /**
+   * Checks if the graph can be u[dated incrementally
+   *
+   * @return
+   */
+  private boolean canUpdateIncrementally(final Graph g) {
+    return PelletOptions.PROCESS_JENA_UPDATES_INCREMENTALLY && !statementDeleted && !changedGraphs.contains(g);
+  }
 
-	public Set<Graph> getChangedGraphs() {
-		Set<Graph> prevLeaves = leafGraphs;
-		
-		leafGraphs = CollectionUtils.makeSet();
-		
-		collectLeafGraphs( rootGraph, prevLeaves );
-		
-		for( Graph prevLeaf : prevLeaves ) {
-			if( !leafGraphs.contains( prevLeaf ) ) {
-				statementDeleted = true;
-				
-				prevLeaf.getEventManager().unregister( this );
-			}
-		}
-		
-		if( statementDeleted ) {
-			return null;
-		}
-		
-		return changedGraphs;
-	}
+  private void collectLeafGraphs(final Graph graph, final Set<Graph> prevLeaves) {
+    if (graph instanceof Polyadic) {
+      final Polyadic union = ((Polyadic) graph);
+      if (union.getBaseGraph() != null)
+        collectLeafGraphs(union.getBaseGraph(), prevLeaves);
 
-	/**
-	 * @return
-	 */
-	public Set<Graph> getLeafGraphs() {
-		return leafGraphs;
-	}
+      for (final Graph graph2 : union.getSubGraphs())
+        collectLeafGraphs(graph2, prevLeaves);
+    } else if (graph instanceof Dyadic) {
+      final Dyadic dyadic = ((Dyadic) graph);
+      if (dyadic.getL() instanceof Graph)
+        collectLeafGraphs((Graph) dyadic.getL(), prevLeaves);
 
-	/**
-	 * Checks if the given triple is an ABox assertion. Currently, only type
-	 * assertions with atomic concepts are detected and property assertions
-	 * 
-	 * @param t
-	 * @return
-	 */
-	private boolean isABoxChange(Triple t) {
-		Node o = t.getObject();
-		Node p = t.getPredicate();
+      if (dyadic.getR() instanceof Graph)
+        collectLeafGraphs((Graph) dyadic.getR(), prevLeaves);
+    } else if (graph instanceof InfGraph) {
+      collectLeafGraphs(((InfGraph) graph).getRawGraph(), prevLeaves);
+    } else if (leafGraphs.add(graph) && !prevLeaves.contains(graph)) {
+      changedGraphs.add(graph);
 
-		// detect if this is a supported ABox type assertion
-		if( p.equals( RDF.type.asNode() ) ) {
-			// check if the object is a bnode to detect complex concepts
-			if( o.isBlank() ) {
-				return false;
-			}
+      graph.getEventManager().register(this);
+    }
+  }
 
-			// check that the object is an atomic concept that exists in the KB
-			ATermAppl object = JenaUtils.makeATerm( o );
-			if( !kb.isClass( object ) ) {
-				return false;
-			}
+  private void deleteABoxTriple(final Triple t) {
+    final ATermAppl s = JenaUtils.makeATerm(t.getSubject());
+    final ATermAppl o = JenaUtils.makeATerm(t.getObject());
 
-			// Note: we do not check if the subject already exists,
-			// as it could be a newly added individual
+    // check if this is a type assertion
+    if (t.getPredicate().equals(RDF.type.asNode())) {
+      if (kb.isIndividual(s))
+        kb.removeType(s, o);
+    } else {
+      // check if the subject is a new individual
+      if (kb.isIndividual(s) && (kb.isIndividual(o) || ATermUtils.isLiteral(o))) {
+        final ATermAppl p = JenaUtils.makeATerm(t.getPredicate());
+        // add the property value
+        kb.removePropertyValue(p, s, o);
+      }
+    }
+  }
 
-		}
-		else {
-			// detect ABox property assertions
-			ATermAppl prop = JenaUtils.makeATerm( p );
+  public void dispose() {
+    for (final Graph graph : leafGraphs) {
+      graph.getEventManager().unregister(this);
+    }
 
-			// check if the role is this is a defined role
-			if( !kb.isProperty( prop ) ) {
-				return false;
-			}
+    leafGraphs.clear();
+    changedGraphs.clear();
 
-			// Note: we do not check if the subject and object already exists,
-			// as they
-			// could be a newly added individuals
-		}
+    statementDeleted = false;
+  }
 
-		return true;
-	}
+  public Set<Graph> getChangedGraphs() {
+    final Set<Graph> prevLeaves = leafGraphs;
 
-	public void notifyAddArray(Graph g, Triple[] triples) {
-		notifyAddIterator( g, IteratorUtils.iterator( triples ) );
-	}
+    leafGraphs = CollectionUtils.makeSet();
 
-	public void notifyAddGraph(Graph g, Graph added) {
-		notifyAddIterator( g, added.find( Triple.ANY ) );
-	}
+    collectLeafGraphs(rootGraph, prevLeaves);
 
-	public void notifyAddIterator(Graph g, Iterator<Triple> it) {
-		boolean canUpdateIncrementally = canUpdateIncrementally( g );
+    for (final Graph prevLeaf : prevLeaves) {
+      if (!leafGraphs.contains(prevLeaf)) {
+        statementDeleted = true;
 
-		if( canUpdateIncrementally ) {
-			while( it.hasNext() ) {
-				Triple t = it.next();
-				if( !isABoxChange( t ) ) {
-					canUpdateIncrementally = false;
-					break;
-				}
-				addABoxTriple( t );
-			}
-		}
-		
-		if( !canUpdateIncrementally ) {
-			changedGraphs.add( g );
-		}
-	}
+        prevLeaf.getEventManager().unregister(this);
+      }
+    }
 
-	public void notifyAddList(Graph g, List<Triple> triples) {
-		notifyAddIterator( g, triples.iterator() );
-	}
+    if (statementDeleted) {
+      return null;
+    }
 
-	public void notifyAddTriple(Graph g, Triple t) {
-		if( canUpdateIncrementally( g ) && isABoxChange( t ) ) {
-			addABoxTriple( t );
-		}
-		else {
-			changedGraphs.add( g );
-		}
-	}
+    return changedGraphs;
+  }
 
-	public void notifyDeleteArray(Graph g, Triple[] triples) {
-		notifyDeleteIterator( g, IteratorUtils.iterator( triples ) );
-	}
+  /**
+   * @return
+   */
+  public Set<Graph> getLeafGraphs() {
+    return leafGraphs;
+  }
 
-	public void notifyDeleteGraph(Graph g, Graph removed) {
-		notifyDeleteIterator( g, removed.find( Triple.ANY ) );
-	}
+  /**
+   * Checks if the given triple is an ABox assertion. Currently, only type assertions with atomic concepts are detected
+   * and property assertions
+   *
+   * @param t
+   * @return
+   */
+  private boolean isABoxChange(final Triple t) {
+    final Node o = t.getObject();
+    final Node p = t.getPredicate();
 
-	public void notifyDeleteIterator(Graph g, Iterator<Triple> it) {
-		boolean canUpdateIncrementally = canUpdateIncrementally( g );
+    // detect if this is a supported ABox type assertion
+    if (p.equals(RDF.type.asNode())) {
+      // check if the object is a bnode to detect complex concepts
+      if (o.isBlank()) {
+        return false;
+      }
 
-		if( canUpdateIncrementally ) {
-			while( it.hasNext() ) {
-				Triple t = it.next();
-				if( !isABoxChange( t ) ) {
-					canUpdateIncrementally = false;
-					break;
-				}
-				deleteABoxTriple( t );
-			}
-		}
-		
-		if( !canUpdateIncrementally ) {
-			statementDeleted = true;
-			changedGraphs.add( g );
-		}
-	}
+      // check that the object is an atomic concept that exists in the KB
+      final ATermAppl object = JenaUtils.makeATerm(o);
+      if (!kb.isClass(object)) {
+        return false;
+      }
 
-	public void notifyDeleteList(Graph g, List<Triple> list) {
-		notifyDeleteIterator( g, list.iterator() );
-	}
+      // Note: we do not check if the subject already exists,
+      // as it could be a newly added individual
 
-	public void notifyDeleteTriple(Graph g, Triple t) {
-		if( canUpdateIncrementally( g ) && isABoxChange( t ) ) {
-			deleteABoxTriple( t );
-		}
-		else {
-			statementDeleted = true;
-			changedGraphs.add( g );
-		}
-	}
+    } else {
+      // detect ABox property assertions
+      final ATermAppl prop = JenaUtils.makeATerm(p);
 
-	public void notifyEvent(Graph source, Object value) {
-		statementDeleted = true;
-	}
+      // check if the role is this is a defined role
+      if (!kb.isProperty(prop)) {
+        return false;
+      }
 
-	public void reset() {
-		changedGraphs.clear();
-		//leafGraphs.clear();
-		statementDeleted = false;
-	}
+      // Note: we do not check if the subject and object already exists,
+      // as they
+      // could be a newly added individuals
+    }
+
+    return true;
+  }
+
+  public boolean isChanged() {
+    if (statementDeleted || !changedGraphs.isEmpty()) {
+      return true;
+    }
+
+    getChangedGraphs();
+
+    return statementDeleted || !changedGraphs.isEmpty();
+  }
+
+  @Override
+  public void notifyAddArray(final Graph g, final Triple[] triples) {
+    notifyAddIterator(g, IteratorUtils.iterator(triples));
+  }
+
+  @Override
+  public void notifyAddGraph(final Graph g, final Graph added) {
+    notifyAddIterator(g, added.find(Triple.ANY));
+  }
+
+  @Override
+  public void notifyAddIterator(final Graph g, final Iterator<Triple> it) {
+    boolean canUpdateIncrementally = canUpdateIncrementally(g);
+
+    if (canUpdateIncrementally) {
+      while (it.hasNext()) {
+        final Triple t = it.next();
+        if (!isABoxChange(t)) {
+          canUpdateIncrementally = false;
+          break;
+        }
+        addABoxTriple(t);
+      }
+    }
+
+    if (!canUpdateIncrementally) {
+      changedGraphs.add(g);
+    }
+  }
+
+  @Override
+  public void notifyAddList(final Graph g, final List<Triple> triples) {
+    notifyAddIterator(g, triples.iterator());
+  }
+
+  @Override
+  public void notifyAddTriple(final Graph g, final Triple t) {
+    if (canUpdateIncrementally(g) && isABoxChange(t)) {
+      addABoxTriple(t);
+    } else {
+      changedGraphs.add(g);
+    }
+  }
+
+  @Override
+  public void notifyDeleteArray(final Graph g, final Triple[] triples) {
+    notifyDeleteIterator(g, IteratorUtils.iterator(triples));
+  }
+
+  @Override
+  public void notifyDeleteGraph(final Graph g, final Graph removed) {
+    notifyDeleteIterator(g, removed.find(Triple.ANY));
+  }
+
+  @Override
+  public void notifyDeleteIterator(final Graph g, final Iterator<Triple> it) {
+    boolean canUpdateIncrementally = canUpdateIncrementally(g);
+
+    if (canUpdateIncrementally) {
+      while (it.hasNext()) {
+        final Triple t = it.next();
+        if (!isABoxChange(t)) {
+          canUpdateIncrementally = false;
+          break;
+        }
+        deleteABoxTriple(t);
+      }
+    }
+
+    if (!canUpdateIncrementally) {
+      statementDeleted = true;
+      changedGraphs.add(g);
+    }
+  }
+
+  @Override
+  public void notifyDeleteList(final Graph g, final List<Triple> list) {
+    notifyDeleteIterator(g, list.iterator());
+  }
+
+  @Override
+  public void notifyDeleteTriple(final Graph g, final Triple t) {
+    if (canUpdateIncrementally(g) && isABoxChange(t)) {
+      deleteABoxTriple(t);
+    } else {
+      statementDeleted = true;
+      changedGraphs.add(g);
+    }
+  }
+
+  @Override
+  public void notifyEvent(final Graph source, final Object value) {
+    statementDeleted = true;
+  }
+
+  public void reset() {
+    changedGraphs.clear();
+    // leafGraphs.clear();
+    statementDeleted = false;
+  }
+
+  public void setEnabled(final boolean enabled) {
+    if (this.enabled == enabled) {
+      return;
+    }
+
+    this.enabled = enabled;
+
+    leafGraphs.clear();
+    changedGraphs.clear();
+
+    statementDeleted = false;
+
+    if (enabled) {
+      collectLeafGraphs(rootGraph, Collections.<Graph> emptySet());
+    } else {
+      for (final Graph graph : leafGraphs) {
+        graph.getEventManager().unregister(this);
+      }
+    }
+  }
 }
